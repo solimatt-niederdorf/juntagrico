cd76a58f1cad4d1056f9bcff23379e35d1773954
Stelian Iancu <stelian.iancu@gmail.com>
2022-11-12 09:51:16 +0100

    Add juntagrico.solimatt.ch to ALLOWED_HOSTS

--- solimatt/settings.py
+++ solimatt/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
 
 DEBUG = os.environ.get("JUNTAGRICO_DEBUG", 'False')=='True'
 
-ALLOWED_HOSTS = ['solimatt.juntagrico.science', 'localhost',]
+ALLOWED_HOSTS = ['solimatt.juntagrico.science', 'localhost', 'juntagrico.solimatt.ch']
 
 
 # Application definition
