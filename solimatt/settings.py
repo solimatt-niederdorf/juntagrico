be6949630e57052616a42fb68b4681b0d3592a13
Peter Spiess <121941646+solimatt@users.noreply.github.com>
2023-12-14 12:45:38 +0100

    Update settings.py

--- solimatt/settings.py
+++ solimatt/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'adminsortable2',
     'solimatt',
     'polymorphic',
-    'juntagrico_billing', # Juntagrico Rechnungs addon
+    'juntagrico_billing',
 ]
 
 ROOT_URLCONF = 'solimatt.urls'
