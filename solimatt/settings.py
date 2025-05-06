9049bf2950174a0c498a1c870fc933ac627007ce
Peter Spiess <121941646+solimatt@users.noreply.github.com>
2025-05-06 15:41:06 +0200

    Update settings.py
    Add this:
    
    TEMPLATES = [
        {
            ...
            'OPTIONS': {
                'context_processors': [
                    ...
                    'juntagrico.context_processors.vocabulary',
                ],
                ...
            },
        },
    ]

--- solimatt/settings.py
+++ solimatt/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.tz',
                 'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
+                'juntagrico.context_processors.vocabulary',
             ],
             'loaders': [
                 'django.template.loaders.filesystem.Loader',
